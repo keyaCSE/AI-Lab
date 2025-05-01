9877908e5b8d469ef03766016a72ffe8b3821901
Yeasmin Kabir Keya <yeasminkabir506@gmail.com>
2025-05-01 13:17:50 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 02:25:38 2024
+
+@author: USER
+"""
+
+"""def printfunction():
+    print("Heloo World")
+    
+def sumfunction(a,b):
+    return (a+b)
+
+printfunction()
+print("sum of two numbers ",sumfunction(45.6,2.3))"""
+
+def sumfunction(a,b):
+    return(a+b)
+
+def sumfunction(a,b):
+    c=a+b
+    
+def printInLoop(l):
+    for x in l:
+        print(x,end=" ")
+        
+l=
+
